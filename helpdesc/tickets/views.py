9c826ef9733f55b89a317265667f033f27fcb3a4
Магомедов Магомедали <magomedovmagomedali@MacBook-Air-Magomedov.local>
2024-04-04 18:20:25 +0300

    Изменен фронт главной страницы со списком тикетов

--- helpdesc/tickets/views.py
+++ helpdesc/tickets/views.py
@@ -36,7 +36,6 @@ class TicketsListView(ListView):
 
     model = Ticket
     template_name = 'tickets/tickets.html'
-    paginate_by = 4
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
